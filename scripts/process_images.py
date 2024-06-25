363424417eb1d4ddbe56d198506961d61029d03c
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-06-25 01:38:47 -0400

    WIP: trying to get the relative hex counts

--- scripts/process_images.py
+++ scripts/process_images.py
@@ -7,6 +7,10 @@ from sklearn.cluster import KMeans
 from collections import Counter
 
 
+def convert_to_hex(rgb):
+    return "#{:02x}{:02x}{:02x}".format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
+
+
 def extract_distinct_colors(image_path, num_colors=10):
     # Load the image
     image = cv2.imread(image_path)
@@ -28,11 +32,14 @@ def extract_distinct_colors(image_path, num_colors=10):
     sorted_colors = sorted(color_counts.items(), key=lambda x: x[1], reverse=True)
 
     # Get the RGB values of the most distinct colors
-    distinct_colors = [
-        kmeans.cluster_centers_[color[0]].astype(int) for color in sorted_colors
-    ]
+    distinct_colors = {
+        convert_to_hex(kmeans.cluster_centers_[color[0]].astype(int)): color[1]
+        for color in sorted_colors
+    }
 
-    return [f"#{r:02x}{g:02x}{b:02x}" for [r, g, b] in distinct_colors]
+    print(distinct_colors)
+
+    # return {f"#{r:02x}{g:02x}{b:02x}" for [r, g, b] in distinct_colors}
 
 
 def process_directory(directory, gun_type):
@@ -43,11 +50,11 @@ def process_directory(directory, gun_type):
             if file.lower().endswith(".png"):
                 print(f"---- {file}")
                 file_path = os.path.join(root, file)
-                colors = extract_distinct_colors(file_path)
-                color_data[pathlib.Path(file).stem] = {
-                    "type": gun_type,
-                    "colors": colors,
-                }
+                colors = extract_distinct_colors(file_path, num_colors=7)
+                # color_data[pathlib.Path(file).stem] = {
+                #     "type": gun_type,
+                #     "colors": colors,
+                # }
     return color_data
 
 
@@ -56,15 +63,15 @@ def main():
     all_colors = {}
 
     for directory in directories:
-        skin_dir = os.path.join("skins", directory)
+        skin_dir = os.path.join("public", "skins", directory)
         if os.path.exists(skin_dir):
             all_colors.update(process_directory(skin_dir, gun_type=directory))
         else:
             print(f"Directory '{directory}' does not exist.")
 
-    # print(all_colors)
-    with open("colors.json", "w") as json_file:
-        json.dump(all_colors, json_file, indent=4)
+    print(all_colors)
+    # with open("colors.json", "w") as json_file:
+    #     json.dump(all_colors, json_file, indent=4)
 
 
 if __name__ == "__main__":
